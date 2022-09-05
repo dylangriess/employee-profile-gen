3ff90fe7c7f47a045f32ae0072a7e089d1ef2910
dylangriess <dylangriess@gmail.com>
2022-09-05 11:24:15 -0600

    deleted style.css to use bootstrap classes only, added styling for finalized product

--- src/output.js
+++ src/output.js
@@ -6,24 +6,20 @@ module.exports = (team) => {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/css/bootstrap-grid.css"
-      integrity="sha512-bNJr+jVWHx74QbOOQeiQmwch8ZZR05dvbytcqZpXSNY0IvN+yhIqHbPnRktg+dDhYGSyDxlXFYWRITSw8uGdiA=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/css/bootstrap.min.css" integrity="sha512-XWTTruHZEYJsxV3W/lSXG1n3Q39YIWOstqvmFsdNEEQfHoZ6vm6E9GK2OrF6DSJSpIbRbi+Nn0WDPID9O7xB2Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="style.css" />
+
     <title>Employee Profiles</title>
 
   </head>
   <body>
-    <header class ="d-flex flex-wrap text-center p-5">
+    <header class="d-flex flex-wrap justify-content-center container-fluid text-light bg-danger p-5">
       <h1>Our Team</h1>
     </header>
-    <main>
-      <section class = "d-flex flex-wrap justify-content-evenly p-5">
+    <main class="container-fluid">
+      <section class = "d-flex flex-wrap justify-content-evenly m-1">
         <!--Employee Cards-->
         ${teamMembers(team)}
 
@@ -39,15 +35,15 @@ module.exports = (team) => {
 </html>`;
 };
 
-const teamMembers = (team) => {
+const teamMembers = (teamMembers) => {
   const generateManager = (manager) => {
     return `
-  <div class="card shadow" style="width: 18rem">
+  <div class="card shadow m-3" style="width: 18rem">
     <div class="card-header bg-primary text-light">
-      <h2>${manager.name}</h2>
-      <div>
-        <h3>Manager</h3>
-        <i class="fa-solid fa-mug-hot m-3"></i>
+    <h2>${manager.name}</h2>
+      <div class="d-flex">
+        <i class="fa-solid fa-mug-hot m-1"></i>
+        <h4 class="m-1">Manager</h3>
       </div>
     </div>
     <ul class="list-group list-group-flush">
@@ -60,30 +56,30 @@ const teamMembers = (team) => {
 
   const generateEngineer = (engineer) => {
     return `
-  <div class="card shadow" style="width: 18rem">
+  <div class="card shadow m-3" style="width: 18rem">
     <div class="card-header bg-primary text-light">
-      <h2>${engineer.name}</h2>
-      <div>
-        <h3>Manager</h3>
-        <i class="fa-solid fa-glasses m-3"></i>
+    <h2>${engineer.name}</h2>
+      <div class="d-flex">
+        <i class="fa-solid fa-glasses m-1"></i>
+        <h4 class="m-1">Engineer</h3>
       </div>
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">ID: ${engineer.id}</li>
       <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
-      <li class="list-group-item">Github: ${engineer.github}</li>
+      <li class="list-group-item">Github: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</li>
     </ul>
   </div>`;
   };
 
   const generateIntern = (intern) => {
     return `
-  <div class="card shadow" style="width: 18rem">
+  <div class="card shadow m-3" style="width: 18rem">
     <div class="card-header bg-primary text-light">
       <h2>${intern.name}</h2>
-      <div>
-        <h3>Manager</h3>
-        <i class="fa-solid fa-graduation-cap m-3"></i>
+      <div class="d-flex">
+        <i class="fa-solid fa-graduation-cap m-1"></i>
+        <h4 class="m-1">Intern</h3>
       </div>
     </div>
     <ul class="list-group list-group-flush">
@@ -95,17 +91,17 @@ const teamMembers = (team) => {
   };
   const cardsArray = [];
   cardsArray.push(
-    team
+    teamMembers
       .filter((employee) => employee.getRole() === "Manager")
       .map((manager) => generateManager(manager))
   );
   cardsArray.push(
-    team
+    teamMembers
       .filter((employee) => employee.getRole() === "Engineer")
       .map((engineer) => generateEngineer(engineer))
   );
   cardsArray.push(
-    team
+    teamMembers
       .filter((employee) => employee.getRole() === "Intern")
       .map((intern) => generateIntern(intern))
   );
